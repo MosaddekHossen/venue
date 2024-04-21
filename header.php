b44fdb4f686fecb325087081bd1a029ba1385758
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-22 02:00:20 +0600

    Added dynamic header

--- header.php
+++ header.php
@@ -24,7 +24,8 @@
                             </div>
                         </a>
                         <nav id="primary-nav" class="dropdown cf">
-                            <ul class="dropdown menu">
+                            <?php venue_menu(); ?>
+                            <!-- <ul class="dropdown menu">
                                 <li class='active'><a href="#">Popular</a></li>
                                 <li><a href="#">Most Visited</a>
                                     <ul class="sub-menu">
@@ -39,7 +40,7 @@
                                 <li><a class="scrollTo" data-scrollTo="blog" href="#">Blog Entries</a></li>
                                 <li><a class="scrollTo" data-scrollTo="services" href="#">Our Services</a></li>
                                 <li><a class="scrollTo" data-scrollTo="contact" href="#">Contact Us</a></li>
-                            </ul>
+                            </ul> -->
                         </nav><!-- / #primary-nav -->
                     </div>
                 </div>
@@ -47,55 +48,4 @@
         </header>
     </div>
 
-    <section class="banner" id="top">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="banner-caption">
-                        <div class="line-dec"></div>
-                        <h2>Best Finder For You</h2>
-                        <span>Suspendisse eu lorem massa. Integer sit amet posuere tellus.</span>
-                        <div class="blue-button">
-                            <a class="scrollTo" data-scrollTo="popular" href="#">Discover More</a>
-                        </div>
-                    </div>
-                    <div class="submit-form">
-                        <form id="form-submit" action="" method="get">
-                            <div class="row">
-                                <div class="col-md-3 first-item">
-                                    <fieldset>
-                                        <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
-                                    </fieldset>
-                                </div>
-                                <div class="col-md-3 second-item">
-                                    <fieldset>
-                                        <input name="location" type="text" class="form-control" id="location" placeholder="Type location..." required="">
-                                    </fieldset>
-                                </div>
-                                <div class="col-md-3 third-item">
-                                    <fieldset>
-                                        <select required name='category' onchange='this.form()'>
-                                            <!-- <select required name='category' onchange='this.form.()'> -->
-                                            <option value="">Select category...</option>
-                                            <option value="Shops">Shops</option>
-                                            <option value="Hotels">Hotels</option>
-                                            <option value="Restaurants">Restaurants</option>
-                                            <option value="Events">Events</option>
-                                            <option value="Meetings">Meetings</option>
-                                            <option value="Fitness">Fitness</option>
-                                            <option value="Cafes">Cafes</option>
-                                        </select>
-                                    </fieldset>
-                                </div>
-                                <div class="col-md-3">
-                                    <fieldset>
-                                        <button type="submit" id="form-submit" class="btn">Search Now</button>
-                                    </fieldset>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    
