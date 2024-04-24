58eece32c585b5e8cfdcbb46ba8a20e87e249357
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-25 02:48:25 +0600

    update

--- header.php
+++ header.php
@@ -25,23 +25,7 @@
                         </a>
                         <nav id="primary-nav" class="dropdown cf">
                             <?php venue_menu(); ?>
-                            <!-- <ul class="dropdown menu">
-                                <li class='active'><a href="#">Popular</a></li>
-                                <li><a href="#">Most Visited</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="#">Most Visited 1</a>
-                                        </li>
-                                        <li><a href="#">Most Visited 2</a>
-                                        </li>
-                                        <li><a href="#">Most Visited 3</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a class="scrollTo" data-scrollTo="blog" href="#">Blog Entries</a></li>
-                                <li><a class="scrollTo" data-scrollTo="services" href="#">Our Services</a></li>
-                                <li><a class="scrollTo" data-scrollTo="contact" href="#">Contact Us</a></li>
-                            </ul> -->
-                        </nav><!-- / #primary-nav -->
+                        </nav>
                     </div>
                 </div>
             </div>
